cf672326867b9cde87377ca1801d323b3128f483
Renata Rainho <renata.rainho95@gmail.com>
2024-07-03 23:02:46 -0300

    atualizações no CRUD do blog pessoal

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.generation.blogpessoal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
+import java.util.List;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	public List <Postagem>findAllByTituloContainingIgnoreCase(@Param("título") String titulo);
 }
